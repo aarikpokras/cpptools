57e5e73b133c06258fac20d90f3b2afd89e7e36e
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-11-14 12:32:06 -0500

    Create amlparse.hpp

---
+++ src/amlparse.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+namespace cpptools{
+    vector<string> get_aml_values(string key, string file) {
+        regex key_r("^" + key + "=");
+        regex sp(" ");
+        string ln;
+        vector<string> ret_vec;
+        ifstream fileTP(file);
+        if (!fileTP.is_open()) {
+            return vector<string>{"err"};
+        }
+        while(getline(fileTP, ln)) {
+            if (regex_search(ln, key_r)) {
+                string ret_vp__ = regex_replace(ln, key_r, "");
+
+                int ret_find = ret_vp__.find(",");
+                ret_vec.push_back(ret_vp__.substr(0, ret_find));
+                ret_vp__.erase(0, ret_find+1);
+
+                ret_vec.push_back(ret_vp__);
+            }
+        }
+        fileTP.close();
+        return ret_vec;
+    }
+}
